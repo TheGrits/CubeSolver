a7debc5f264cb5f2d4bed6e384a99b5e0aa1518b
The_Grits <gritsprogramming@gmail.com>
2015-09-14 12:16:02 -0400

    Update main.js
    Got rid of bad code again.

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 var c = document.getElementById("mainCanvas");
-var ctx = c.getContext("2d");
-ctx.font = "50px Arial";
-ctx.strokeText("Canvas Test", 10, 10);
-ctx.strokeStyle="Red";
-ctx.stroke();
+// shoot. Still wasn't working, got rid of it again.
